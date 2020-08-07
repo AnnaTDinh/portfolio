e9e73934287cdcb86cd68da3eb53603f15dbfa70
Anna Dinh <Annathuydinh@gmail.com>
2020-08-07 15:11:05 -0700

    Navbar that links to other page paths

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Nav() {
+const Nav = () => {
   return (
-    <div className="nav">
+    <div className="Nav">
       Navbar
+      <Link to="/about">About</Link>
+      <Link to="/resume">Resume</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/technical">Technical Skills</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   )
 }
