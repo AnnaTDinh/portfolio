40367cb527340840d0128dcb0a28f0c7c8ac2d64
Anna Dinh <Annathuydinh@gmail.com>
2020-08-07 14:03:16 -0700

    added file-loader, css-loader, style-loader and html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|svg|JPG|png)$/,
+        loader: "file-loader",
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(`${DIST_DIR}/index.html`),
+      filename: 'index.html'
+    })
+  ]
+};
