7a049e1ef4e87d9eb54147cd9ecb7faa92c9ff34
Anna Dinh <Annathuydinh@gmail.com>
2020-08-13 21:32:46 -0700

    Removed the About Me path

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,12 +4,20 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <div className="Nav">
-      Navbar
-      <Link to="/about">About</Link>
-      <Link to="/resume">Resume</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/technical">Technical Skills</Link>
-      <Link to="/contact">Contact</Link>
+      <p className="Name">Anna Dinh</p>
+      <nav>
+        <ul className="Paths">
+          <li><Link to="/resume">Resume</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/technical">Technical Skills</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      
+      
+      
+      
+      
     </div>
   )
 }
