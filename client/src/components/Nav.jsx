93520325dc5005bb96b5823a9777aef53324f64f
Anna Dinh <Annathuydinh@gmail.com>
2020-08-13 21:52:41 -0700

    added a horizontal line after the Navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <div className="Nav">
-      <p className="Name"><Link to="/">Anna Dinh</Link></p>
-      <nav>
-        <ul className="Paths">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/technical">Technical Skills</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      
-      
-      
-      
-      
+    <div>
+      <div className="Nav">
+        <p className="Name"><Link to="/">Anna Dinh</Link></p>
+        <nav>
+          <ul className="Paths">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/technical">Technical Skills</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <hr/>
     </div>
   )
 }
