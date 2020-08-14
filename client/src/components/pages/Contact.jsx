8e5ea129759011f1ef249ced770315d15c64bccb
Anna Dinh <Annathuydinh@gmail.com>
2020-08-14 01:41:28 -0700

    Added basic info to the contact page

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -7,7 +7,14 @@ const Contact = () => {
     <div>
       <Nav/>
       <div className="Contact">
-        Contact
+        <h3>Get in touch</h3>
+        <p>If you want to get in touch or just say hi, you can leave a message below</p>
+        {/* insert form */}
+      </div>
+      <div>
+        <h3>Social</h3>
+        <p>Feel free to check out my Github and Linkedin</p>
+        {/* insert linked in and github svg with redirects */}
       </div>
     </div>
   )
