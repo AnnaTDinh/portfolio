7942bf3c7f72d118c5870a43ce3f207d25a99033
Anna Dinh <Annathuydinh@gmail.com>
2020-08-19 00:07:45 -0700

    Imported lazy and Suspense from react

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-
+import React, { lazy, Suspense } from "react";
 //pages
-import MainPage from './pages/MainPage.jsx';
-import PageNotFound from './pages/PageNotFound.jsx';
-import Resume from './pages/Resume.jsx';
-import Projects from './pages/Projects.jsx';
-import TechnicalSkills from './pages/TechnicalSkills.jsx';
-import Contact from './pages/Contact.jsx';
+import MainPage from "./pages/MainPage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Resume from "./pages/Resume.jsx";
+import Projects from "./pages/Projects.jsx";
+import TechnicalSkills from "./pages/TechnicalSkills.jsx";
+import Contact from "./pages/Contact.jsx";
+import AboutMe from "./pages/AboutMe.jsx";
 
 import "../../dist/styles.css";
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
   render() {
     return (
-      <div>
+      <div id="Portfolio">
         <Router>
           <Switch>
-           <Route exact path="/" component={MainPage}/>
-            <Route path="/404" component={PageNotFound}/>
-            <Route exact path="/resume" component={Resume}/>
-            <Route exact path="/projects" component={Projects}/>
-            <Route exact path="/technical" component={TechnicalSkills}/>
-            <Route exact path="/contact" component={Contact}/>
-            <Redirect to="/404"/>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/about" component={AboutMe} />
+            <Route path="/404" component={PageNotFound} />
+            <Route exact path="/resume" component={Resume} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/technical" component={TechnicalSkills} />
+            <Route exact path="/contact" component={Contact} />
+            <Redirect to="/404" />
           </Switch>
         </Router>
       </div>
-      // <div className="Main">
-      //   Main
-      //   <Nav/>
-      // </div>
-    )
+    );
   }
 }
 
