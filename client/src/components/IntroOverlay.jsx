4596bb099f23184cb3c785df4e72486cdbcbf126
Anna Dinh <Annathuydinh@gmail.com>
2020-08-19 00:17:39 -0700

    Removed an empty tag

--- client/src/components/IntroOverlay.jsx
+++ client/src/components/IntroOverlay.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 const IntroOverlay = () => {
   return (
-    <>
-      <div className='intro-overlay'>
-        <div className='top'>
-          <div className='overlay-top'></div>
-          <div className='overlay-top'></div>
-          <div className='overlay-top'></div>
-        </div>
+    <div className="intro-overlay">
+      <div className="top">
+        <div className="overlay-top"></div>
+        <div className="overlay-top"></div>
+        <div className="overlay-top"></div>
       </div>
-    </>
+    </div>
   );
 };
 
