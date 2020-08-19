7cae783e960ebb01fda63041076da5924b5feeee
Anna Dinh <Annathuydinh@gmail.com>
2020-08-19 00:08:22 -0700

    Added a main page link/path

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
         </div>
           <ul className="Paths">
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
             <li><Link to="/resume">Resume</Link></li>
             <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/technical">Technical Skills</Link></li>
